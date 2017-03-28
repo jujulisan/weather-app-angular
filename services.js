2416d31074f9d3ddc7c4fe67a7635914e54fe51f
Juliane Lima <jujulisan@gmail.com>
2017-03-28 12:21:56 -0300

    Designed services

--- services.js
+++ services.js
@@ -4,3 +4,12 @@ weatherApp.service('cityService', function(){
     this.city = "New York, NY";
     
 });
+
+weatherApp.service('weatherService', ['$resource', function($resource){
+    
+    this.GetWeather = function(city, days){
+       var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=685ed96e4e91ba38787c55c481e948d0", {  callback: "JSON_CALLBACK" }, { get: {method: "JSONP"}});
+
+        return weatherAPI.get({ q: city, cnt: days });
+    };
+}]);
